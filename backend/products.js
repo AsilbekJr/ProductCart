2f4bd91b080ae28efe1b2e55ad52dee4fbdf3e74
AsilbekJr <89327572+AsilbekJr@users.noreply.github.com>
2022-11-05 20:30:17 +0500

    product cart updated

---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "redmi 9c",
+    desc: "lwerwe ew rwr ewrewrwer",
+    price: 299,
+    image:
+      "https://res.cloudinary.com/dkmi1dgqv/image/upload/v1663256575/online-shop/gsmarena_001_lpnumv.jpg",
+  },
+  {
+    id: 2,
+    name: "Samsung A23",
+    desc: "lwerwe ew rwr ewrewrwer",
+    price: 200,
+    image:
+      "https://res.cloudinary.com/dkmi1dgqv/image/upload/v1663256681/online-shop/xiaomi-redmi-9c-2gb-32gb-6.53-dual-sim-smartphone_emim51.jpg",
+  },
+  {
+    id: 3,
+    name: "Iphone 13pro ",
+    desc: "lwerwe ew rwr ewrewrwer",
+    price: 179,
+    image:
+      "https://res.cloudinary.com/dkmi1dgqv/image/upload/v1663256782/online-shop/iphone-13-pro-max-gold-select_ozwvpk.png",
+  },
+];
+
+module.exports = products;
